ebf802b5968944c0241052369c968001debca8f3
Jaeroine Mae Catacutan <35668936+jamiecatt@users.noreply.github.com>
2018-03-27 10:25:55 -0700

    Wrong Turn

--- wrong-turn.ino
+++ wrong-turn.ino
@@ -3,16 +3,17 @@ void setup() {
   pinMode(5, OUTPUT);
   pinMode(9, OUTPUT);
 }
-//First ledPin blinks fast to signify wrong turn. Second ledPin blinks normally to signify following directions.
+//First Red ledPin blinks fast to signify wrong turn. Second Yellow ledPin blinks normally to signify following directions.
 // The loop function runs over and over again forever
 void loop() {
   digitalWrite(5, HIGH);   // Turn the LED on (HIGH is the voltage level)
-  delay(100);                
+  delay(50);                
   digitalWrite(5, LOW);    // Turn the LED off by making the voltage LOW
-  delay(50);        
+  delay(20);        
     
   digitalWrite(9, HIGH);   // Turn the LED on (HIGH is the voltage level)
-  delay(300);                  
+  delay(500);                  
   digitalWrite(9, LOW);    // Turn the LED off by making the voltage LOW
   delay(300); 
+
 }
