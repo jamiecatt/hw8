306e377cdf3543dffc1aaed8a72c3b5acbd95937
Jaeroine Mae Catacutan <35668936+jamiecatt@users.noreply.github.com>
2018-03-27 10:17:14 -0700

    Overdue

--- overdue.ino
+++ overdue.ino
@@ -6,7 +6,7 @@ void setup() {
 // The loop function runs over and over again forever
 void loop() {
   digitalWrite(5, HIGH);   // Turn the LED on (HIGH is the voltage level)
-  delay(300);                 // Fast blink to notify overdue assignment.      
+  delay(100);                 // Fast blink to notify overdue assignment.      
   digitalWrite(5, LOW);    // Turn the LED off by making the voltage LOW
-  delay(100);                    
+  delay(50);                    
 }
