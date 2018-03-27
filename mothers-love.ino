3679b443867dc2b026b1607b9f8cb74e3342a935
Jaeroine Mae Catacutan <35668936+jamiecatt@users.noreply.github.com>
2018-03-27 10:18:29 -0700

    Mother's Love

---
+++ mothers-love.ino
@@ -0,0 +1,18 @@
+void setup() {
+  // Initialize digital pin LED_BUILTIN as an output.
+  pinMode(5, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+// Two ledPins blinking simultaneously to signify love.
+// The loop function runs over and over again forever
+void loop() {
+  digitalWrite(5, HIGH);   // Turn the LED on (HIGH is the voltage level)
+  delay(100);                 // Fast blink to notify overdue assignment.      
+  digitalWrite(5, LOW);    // Turn the LED off by making the voltage LOW
+  delay(50);        
+
+  digitalWrite(9, HIGH);   // Turn the LED on (HIGH is the voltage level)
+  delay(300);                 // Fast blink to notify overdue assignment.      
+  digitalWrite(9, LOW);    // Turn the LED off by making the voltage LOW
+  delay(100);     
+}
