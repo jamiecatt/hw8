c1e9e62f18ab76a94ab32c8f0a16113a78dd0a58
Jaeroine Mae Catacutan <35668936+jamiecatt@users.noreply.github.com>
2018-03-27 10:20:56 -0700

    Mother's Love

--- mothers-love.ino
+++ mothers-love.ino
@@ -7,12 +7,12 @@ void setup() {
 // The loop function runs over and over again forever
 void loop() {
   digitalWrite(5, HIGH);   // Turn the LED on (HIGH is the voltage level)
-  delay(100);                 // Fast blink to notify overdue assignment.      
+  delay(100);                 
   digitalWrite(5, LOW);    // Turn the LED off by making the voltage LOW
   delay(50);        
 
   digitalWrite(9, HIGH);   // Turn the LED on (HIGH is the voltage level)
-  delay(300);                 // Fast blink to notify overdue assignment.      
+  delay(300);        
   digitalWrite(9, LOW);    // Turn the LED off by making the voltage LOW
   delay(100);     
 }
